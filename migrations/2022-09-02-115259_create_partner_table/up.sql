e2fe80f0748cc3a5550301129ec24ba6220490ba
ddanielsantos <contatodanieljob@gmail.com>
2022-09-02 17:18:01 -0300

    refactor: drop geometry field by now

--- migrations/2022-09-02-115259_create_partner_table/up.sql
+++ migrations/2022-09-02-115259_create_partner_table/up.sql
@@ -4,7 +4,5 @@ CREATE TABLE partner (
   id            VARCHAR (36) PRIMARY KEY,
   tradingName   VARCHAR (256) NOT NULL,
   ownerName     VARCHAR (256) NOT NULL,
-  document      VARCHAR (256) NOT NULL,
-  coverageArea  GEOMETRY NOT NULL,
-  address       GEOMETRY NOT NULL
+  document      VARCHAR (256) NOT NULL
 );
