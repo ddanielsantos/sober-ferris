881325f96a1059215c88329aecb2c2b8cce4760c
ddanielsantos <contatodanieljob@gmail.com>
2022-09-02 09:53:09 -0300

    feat: setup partner table migrations

--- src/schema.rs
+++ src/schema.rs
@@ -1,5 +1,25 @@
 // @generated automatically by Diesel CLI.
 
+pub mod sql_types {
+    #[derive(diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "geometry"))]
+    pub struct Geometry;
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::Geometry;
+
+    partner (id) {
+        id -> Varchar,
+        tradingname -> Varchar,
+        ownername -> Varchar,
+        document -> Varchar,
+        coveragearea -> Geometry,
+        address -> Geometry,
+    }
+}
+
 diesel::table! {
     spatial_ref_sys (srid) {
         srid -> Int4,
@@ -9,3 +29,8 @@ diesel::table! {
         proj4text -> Nullable<Varchar>,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    partner,
+    spatial_ref_sys,
+);
