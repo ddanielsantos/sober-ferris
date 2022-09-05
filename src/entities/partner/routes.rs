78470687176031ac13092be3c58d358366ac8171
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2022-09-05 20:43:37 -0300

    refactor: resolve folder structure (#6)
    * refactor: move partner model
    
    * refactor: move partner routes
    
    * refactor: resolve new modules

--- src/entities/partner/routes.rs
+++ src/entities/partner/routes.rs
@@ -1,14 +1,14 @@
 use diesel::prelude::*;
+use super::model::Partner;
 use rocket::serde::json::Json;
 use crate::establish_connection;
-use crate::models::partner::Partner;
-use crate::schema::partner::dsl as partner_dsl;
+use crate::schema::partner::dsl;
 
 #[get("/<id>")]
 pub fn partner_by_id (id: &str) -> Json<Partner> {
     let connection = &mut establish_connection();
-    let result = partner_dsl::partner
-        .filter(partner_dsl::id.eq(id))
+    let result = dsl::partner
+        .filter(dsl::id.eq(id))
         .first::<Partner>(connection);
 
     let partners = result.unwrap();
