a3acab7d6a782faab5b9927b64d15dd483bdc85e
ddanielsantos <contatodanieljob@gmail.com>
2022-09-07 18:14:09 -0300

    feat: remove nullable

--- src/schema.rs
+++ src/schema.rs
@@ -1,25 +1,21 @@
 // @generated automatically by Diesel CLI.
 
+pub mod sql_types {
+    #[derive(diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "geometry"))]
+    pub struct Geometry;
+}
+
 diesel::table! {
+    use diesel::sql_types::*;
+    use postgis_diesel::sql_types::*;
+    use super::sql_types::Geometry;
+
     partner (id) {
         id -> Varchar,
         trading_name -> Varchar,
         owner_name -> Varchar,
         document -> Varchar,
+        address -> Geometry,
     }
 }
-
-diesel::table! {
-    spatial_ref_sys (srid) {
-        srid -> Int4,
-        auth_name -> Nullable<Varchar>,
-        auth_srid -> Nullable<Int4>,
-        srtext -> Nullable<Varchar>,
-        proj4text -> Nullable<Varchar>,
-    }
-}
-
-diesel::allow_tables_to_appear_in_same_query!(
-    partner,
-    spatial_ref_sys,
-);
